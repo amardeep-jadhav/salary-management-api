90e3f2fe66a2a39e07ff08811de704c962a31240
Amardeep <amardeep129@gmail.com>
2026-04-16 23:23:23 +0530

    test: red — employee model validations

---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Employee, type: :model do
+  describe 'validations' do
+    subject { build(:employee) }
+
+    it { should validate_presence_of(:full_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:salary) }
+    it { should validate_presence_of(:currency) }
+    it { should validate_presence_of(:hired_on) }
+    it { should validate_presence_of(:employment_type) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should validate_numericality_of(:salary).is_greater_than(0) }
+
+    it { should validate_inclusion_of(:employment_type).in_array(%w[full_time part_time contract]) }
+  end
+end
