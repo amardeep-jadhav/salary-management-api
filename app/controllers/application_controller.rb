fc54bbd0b042dd3273d849dd77db12311233ca79
Amardeep <amardeep129@gmail.com>
2026-04-16 22:48:08 +0530

    chore: add CORS, Pagy initializers and ApplicationController base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  include Pagy::Backend
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::ParameterMissing, with: :bad_request
+
+  private
+
+  def not_found(e)
+    render json: { error: e.message }, status: :not_found
+  end
+
+  def bad_request(e)
+    render json: { error: e.message }, status: :bad_request
+  end
 end
